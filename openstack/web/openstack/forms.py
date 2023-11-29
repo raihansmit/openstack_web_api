6f9451f942c5d7207a0ed194767d2ff80ac063e4
raihansmit <raihankaeni129@gmail.com>
2023-11-29 15:24:42 +0700

    update 3

---
+++ openstack/web/openstack/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from openstack.models import instance
+
+
+class instanceForm(forms.ModelForm):
+    class Meta:
+        model = instance
+        fields = '__all__'
+
+class instanceFormMahasiswa(forms.ModelForm):
+    class Meta:
+        model = instance
+        fields = ['name', 'fk_flavor','fk_images', 'tujuan']
